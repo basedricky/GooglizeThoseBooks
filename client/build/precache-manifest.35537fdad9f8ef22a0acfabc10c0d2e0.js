d211a71b71158e5dbdecde699772a6b2b79e5d43
Ricky Panh <ricky@dragonflyut.com>
2021-05-12 20:53:45 -0600

    deplying unfinished app

---
+++ client/build/precache-manifest.35537fdad9f8ef22a0acfabc10c0d2e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "756ad72bbf07e5c06193186eeafee42f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d98c7cb2476bc21050cd",
+    "url": "/static/css/main.ebc7a96f.chunk.css"
+  },
+  {
+    "revision": "8413eee26aafef02e9b8",
+    "url": "/static/js/2.697fca5e.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.697fca5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d98c7cb2476bc21050cd",
+    "url": "/static/js/main.3690a8c1.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "15f6f6721809e8b218c4d9b62b64803d",
+    "url": "/static/media/library.15f6f672.jpg"
+  }
+]);
